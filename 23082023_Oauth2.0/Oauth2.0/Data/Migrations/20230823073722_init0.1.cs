9ce9ea4449740e363ad6506244fda21cfdd43d50
Ali Kemal SARI <a.kemalsari46@gmail.com>
2023-08-23 14:37:33 +0300

    Created and uploaded

---
+++ 23082023_Oauth2.0/Oauth2.0/Data/Migrations/20230823073722_init0.1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Oauth2._0.Data.Migrations
+{
+    public partial class init01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
